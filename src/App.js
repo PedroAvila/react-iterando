ed2778c59b5dc8ca11af3bedd79589eabab6ad00
PedroAvila <minimainor_austin@hotmail.com>
2021-05-19 15:23:28 -0500

    Integrar librerias de terceros usando las refs de React

--- src/App.js
+++ src/App.js
@@ -1,31 +1,59 @@
 
 import React, { Component } from 'react'
+import Chart from 'chart.js';
 
-class Entrada extends Component {
+class Graficas extends Component {
+  grafica = React.createRef()
 
-  entrada = React.createRef()
-
-  componentDidMount () {
-    this.focus()
-  }
-
-  focus = () => {
-    this.entrada.current.focus()
-  }
-  blur = () => {
-    this.entrada.current.blur()
+  componentDidMount() {
+    const ctx = this.grafica.current.getContext('2d')
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 5, 2, 3],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    })
   }
 
   render() {
-    return(
+    return (
       <div>
-        <input type="text" ref={ this.entrada } />
-        <button onClick={ this.focus }>
-          Focus
-        </button>
-        <button onClick={ this.blur }>
-          Blur
-        </button>
+        <canvas
+          ref={this.grafica}
+          width="400"
+          height="400"
+        >
+        </canvas>
       </div>
     )
   }
@@ -34,15 +62,15 @@ class Entrada extends Component {
 
 class App extends Component {
 
-  render () {
-    
-    return(
+  render() {
+
+    return (
       <div>
-        <h1>React refs</h1>
-        <Entrada />
+        <h1>Usando refs</h1>
+        <Graficas />
       </div>
     )
   }
-}  
+}
 
 export default App;
---
+++ src/App6.js
@@ -0,0 +1,48 @@
+
+import React, { Component } from 'react'
+
+class Entrada extends Component {
+
+  entrada = React.createRef()
+
+  componentDidMount () {
+    this.focus()
+  }
+
+  focus = () => {
+    this.entrada.current.focus()
+  }
+  blur = () => {
+    this.entrada.current.blur()
+  }
+
+  render() {
+    return(
+      <div>
+        <input type="text" ref={ this.entrada } />
+        <button onClick={ this.focus }>
+          Focus
+        </button>
+        <button onClick={ this.blur }>
+          Blur
+        </button>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+
+  render () {
+    
+    return(
+      <div>
+        <h1>React refs</h1>
+        <Entrada />
+      </div>
+    )
+  }
+}  
+
+export default App;
