f24a0e616a2d2c3e132b076d6d15e514791b9a9e
PedroAvila <minimainor_austin@hotmail.com>
2021-05-20 10:02:48 -0500

    Ejemplo de formulario con opciones de Selección Multiple

--- src/App12.js
+++ src/App12.js
@@ -6,19 +6,13 @@ import React, { Component } from 'react'
 class App extends Component {
 
   state= {
-    techs: ["Vue"]
+    tech: "Vue"
   }
 
   handleChange = (event) => {
-
-    const techs = Array.from(
-      event.target.selectedOptions,
-      option => option.value
-    )
-  
-
-    this.setState({ techs })
-
+    this.setState({
+      tech: event.target.value
+    })
   }
 
   render() {
@@ -27,27 +21,16 @@ class App extends Component {
       <div>
         <h1>
           Etiqueta Select 
-          { this.state.techs }
+          { this.state.tech }
         </h1>
         <form>
-          <select 
-            value={ this.state.techs } 
-            onChange={ this.handleChange }
-            multiple
-          >
+          <select value={ this.state.tech } onChange={ this.handleChange }>
             <option value="Angular">Angular</option>
             <option value="React">React</option>
             <option value="Vue">Vue</option>
             <option value="Vanilla">Vanilla</option>
           </select>
         </form>
-        <ul>
-          { this.state.techs.map(tech=> (
-            <li key={ tech }>
-              { tech }
-            </li>
-          )) }
-        </ul>
       </div>
     )
   }
