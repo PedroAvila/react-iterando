ff17edddc44ee50374affd9c3f080f94440941d2
PedroAvila <minimainor_austin@hotmail.com>
2021-05-21 15:24:13 -0500

    Solución a tarea (buscador de películas)

--- src/App.js
+++ src/App.js
@@ -5,28 +5,31 @@ import React, { Component } from 'react'
 class App extends Component {
 
   state = {
-    movie: {}
+    movie: {},
+    isFetching: false 
   }
 
-  handleSumit = (event) => {
+  handleSubmit = (event) => {
     event.preventDefault()
+
+    this.setState({ isFetching: true })
+
     const title = event.target[0].value
 
     const url = "http://www.omdbapi.com/?i=tt3896198&apikey=99983c30"
     
     fetch(url + "&t=" + title)
     .then(res => res.json())
-    .then(movie => this.setState({ movie }))
+    .then(movie => this.setState({ movie, isFetching: false }))
   }
 
   render() {
 
-    const { movie } = this.state
+    const { movie, isFetching } = this.state
 
     return (
       <div>
-        
-        <form onSubmit={ this.handleSumit }>
+        <form onSubmit={ this.handleSubmit }>
           <input 
             type="text"
             placeholder="Nombre de Pelicula" 
@@ -35,19 +38,24 @@ class App extends Component {
             Buscar
           </button>
         </form>
-        <div>
-          <h1>{ movie.Title }</h1>
-          <p>
-            { movie.Plot }
-          </p>
-          <img 
-            src={ movie.Poster } 
-            alt="Poster" 
-            style={{
-              width: "150px"
-            }}
-          />
-        </div>
+          { isFetching && (
+            <h2>Cargando...</h2>
+          ) }
+          { movie.Title && !isFetching && (
+            <div>
+            <h1>{ movie.Title }</h1>
+            <p>
+              { movie.Plot }
+            </p>
+            <img 
+              src={ movie.Poster } 
+              alt="Poster" 
+              style={{
+                width: "150px"
+              }}
+            />
+          </div>
+        ) }
       </div>
     )
   }
