23fba8e2cf2b1be1e79d7a764fd689250151b1ca
PedroAvila <minimainor_austin@hotmail.com>
2021-05-21 14:44:48 -0500

    Ejemplo buscador de peliculas

---
+++ src/App16.js
@@ -0,0 +1,49 @@
+
+import React, { Component } from 'react'
+
+
+class App extends Component {
+
+  state = {
+    users: [],
+    cargando: true
+  }
+
+  componentDidMount(){
+    fetch("https://jsonplaceholder.typicode.com/users")
+    .then(res => res.json())
+    .then(users => this.setState({ users, cargando: false }))
+    .catch(error=> {
+      // Manejo del error
+      
+    })
+  }
+
+
+  render() {
+
+    if (this.state.cargando) {
+      return <h1>Cargando...</h1>
+    }
+
+    return (
+      <div>
+        <h1>Petición HTTP</h1>
+        <h2>{ this.state.text }</h2>
+        <ul>
+          { this.state.users.map(user =>(
+            <li key={ user.id }>
+              { user.name }
+              <a href={ `https://${ user.website }` }>
+                Website
+              </a>
+            </li>
+          ))}
+        </ul>
+
+      </div>
+    )
+  }
+}
+
+export default App;
